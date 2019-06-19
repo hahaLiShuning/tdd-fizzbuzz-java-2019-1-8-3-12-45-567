aaa4b46ebd527afa74c3162e3562a8dcb8a3e61b
LIShuning <lishiuning1955142623@outlook.com>
2019-06-19 13:41:50 +0800

    game completed

--- src/main/java/com/thoughtworks/tdd/Game.java
+++ src/main/java/com/thoughtworks/tdd/Game.java
@@ -1,4 +1,37 @@
 package com.thoughtworks.tdd;
 
 public class Game {
+
+    public void startGame() {
+        for (int i = 1; i <= 120; i++) {
+            String result = generateResult(i);
+            System.out.println(result);
+        }
+    }
+
+    public String generateResult(int i) {
+        String fizzStr = fizz(i);
+        String buzzStr = buzz(i);
+        String whizzStr = whizz(i);
+        String result;
+
+        if (fizzStr.equals("") && buzzStr.equals("") && whizzStr.equals("")) {
+            result = String.valueOf(i);
+        } else {
+            result = fizzStr.concat(buzzStr).concat(whizzStr);
+        }
+        return result;
+    }
+
+    public String fizz(int i) {
+        return i % 3 == 0 ? "Fizz" : "";
+    }
+
+    public String buzz(int i) {
+        return i % 5 == 0 ? "Buzz" : "";
+    }
+
+    public String whizz(int i) {
+        return i % 7 == 0 ? "Whizz" : "";
+    }
 }
--- src/main/java/com/thoughtworks/tdd/GameMain.java
+++ src/main/java/com/thoughtworks/tdd/GameMain.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.tdd;
 
 public class GameMain {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
 }
