c9a184a4a10f0755fbd1a7e9f519bce309379ff0
LIShuning <lishiuning1955142623@outlook.com>
2019-06-19 13:41:33 +0800

    test for game completed

---
+++ src/test/java/com/thoughtworks/tdd/GameTest.java
@@ -0,0 +1,83 @@
+package com.thoughtworks.tdd;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void should_return_Fizz_when_it_is_only_the_multiple_of_3() {
+        int [] testNums = { 3, 6 };
+        String expected = "Fizz";
+        run_when_it_is_only_one_type_of_special_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_Buzz_when_it_is_only_the_multiple_of_5() {
+        int [] testNums = { 5, 10 };
+        String expected = "Buzz";
+        run_when_it_is_only_one_type_of_special_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_Whizz_when_it_is_only_the_multiple_of_7() {
+        int [] testNums = { 7, 14 };
+        String expected = "Whizz";
+        run_when_it_is_only_one_type_of_special_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_it_is_the_multiple_of_3_and_5() {
+        int [] testNums = {15, 30};
+        String expected = "FizzBuzz";
+        run_when_it_is_two_type_of_speciall_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_it_is_the_multiple_of_3_and_7() {
+        int [] testNums = {21, 42};
+        String expected = "FizzWhizz";
+        run_when_it_is_two_type_of_speciall_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_it_is_the_multiple_of_5_and_7() {
+        int [] testNums = {35, 70};
+        String expected = "BuzzWhizz";
+        run_when_it_is_two_type_of_speciall_number(testNums, expected);
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_when_it_is_the_multiple_of_3_5_and_7() {
+        int [] testNums = {105, 210};
+        String expected = "FizzBuzzWhizz";
+        String result1 = game.generateResult(testNums[0]);
+        String result2 = game.generateResult(testNums[1]);
+
+        Assert.assertEquals(result1, expected);
+        Assert.assertEquals(result2, expected);
+    }
+
+    private void run_when_it_is_only_one_type_of_special_number(int[] testNums, String expected) {
+        String result1 = game.generateResult(testNums[0]);
+        String result2 = game.generateResult(testNums[1]);
+
+        Assert.assertEquals(result1, expected);
+        Assert.assertEquals(result2, expected);
+    }
+
+    private void run_when_it_is_two_type_of_speciall_number(int [] testNums, String expected) {
+        String result1 = game.generateResult(testNums[0]);
+        String result2 = game.generateResult(testNums[1]);
+
+        Assert.assertEquals(result1, expected);
+        Assert.assertEquals(result2, expected);
+    }
+}
